a0c2aec1bd7edc0d7a659354b591e00e4e284ab3
“Lukas” <info@brainlesslukas.xyz>
2025-09-28 17:13:56 +0200

    Button setup

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,7 @@
-const Button = () => {
-  return <div>Button</div>;
+import type React from "react";
+
+const Button = ({ children, ...props }: React.ComponentProps<"button">) => {
+  return <button {...props}>{children}</button>;
 };
 
 export default Button;
