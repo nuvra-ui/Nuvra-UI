78dcd6aa89b68b8c688cbed9917d44244bf39afa
“Lukas” <info@brainlesslukas.xyz>
2025-09-28 18:26:29 +0200

    Button Small Variant

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,21 +2,25 @@ import { cva, type VariantProps } from "class-variance-authority";
 import type React from "react";
 import { twMerge } from "tailwind-merge";
 
-const buttonVariants = cva("rounded-lg flex items-center justify-center", {
-  variants: {
-    variant: {
-      default: "bg-black text-white hover:bg-black/85",
-      link: "bg-transparent text-black hover:underline underline-offset-3",
+const buttonVariants = cva(
+  "rounded-lg text-base flex items-center justify-center",
+  {
+    variants: {
+      variant: {
+        default: "bg-black text-white hover:bg-black/85",
+        link: "bg-transparent text-black hover:underline underline-offset-3",
+      },
+      size: {
+        default: "h-9 px-4 py-2",
+        sm: "h-8 px-2 py-1 text-sm rounded-md",
+      },
     },
-    size: {
-      default: "h-9 px-4 py-2",
+    defaultVariants: {
+      variant: "default",
+      size: "default",
     },
-  },
-  defaultVariants: {
-    variant: "default",
-    size: "default",
-  },
-});
+  }
+);
 
 const Button = ({
   className,
