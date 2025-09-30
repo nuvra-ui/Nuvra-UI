df0a5da2d2e1ecc989b3c86e34ab40e6ce2172ca
“Lukas” <info@brainlesslukas.xyz>
2025-09-30 23:19:12 +0200

    Input Design

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,5 +1,20 @@
-const Input = () => {
-  return <div>Input</div>;
+import { twMerge } from "tailwind-merge";
+
+const Input = ({
+  className,
+  type,
+  ...props
+}: React.ComponentProps<"input">) => {
+  return (
+    <input
+      className={twMerge(
+        "w-full h-11 p-3 text-sm border placeholder:text-black bg-gray-50 border-gray-400 focus:ring-2 focus:ring-gray-600 focus:border-none outline-none rounded-sm",
+        className
+      )}
+      type={type}
+      {...props}
+    />
+  );
 };
 
-export default Input;
+export { Input };
