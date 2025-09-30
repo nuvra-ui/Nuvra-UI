a1452346857fa7ef20a8e9b58349c36f8fa5dc68
“Lukas” <info@brainlesslukas.xyz>
2025-09-30 22:39:20 +0200

    Implement Input-Component to Storybook

---
+++ src/stories/Input.stories.tsx
@@ -0,0 +1,11 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import Input from "../components/Input/Input";
+
+const meta = {
+  component: Input,
+} satisfies Meta<typeof Input>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const InputExample: Story = {};
