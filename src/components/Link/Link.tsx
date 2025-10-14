2aeb20bca88b2b7ac45d12417d8d2bdd5eb01845
“Lukas” <info@brainlesslukas.xyz>
2025-10-14 22:11:32 +0200

    Design Link

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,5 +1,11 @@
-const Link = () => {
-  return <div>Link</div>;
+import React from "react";
+
+const Link = ({ children }: React.ComponentProps<"button">) => {
+  return (
+    <a className="bg-transparent text-primary hover:underline underline-offset-3">
+      {children}
+    </a>
+  );
 };
 
 export { Link };
