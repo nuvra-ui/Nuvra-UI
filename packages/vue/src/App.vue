ec65756fbe953b129daf06c39420bd4dd831acb6
Lukas <info@brainlesslukas.xyz>
2025-12-27 23:11:19 +0100

    fix: remove demo Component

--- packages/vue/src/App.vue
+++ packages/vue/src/App.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
 import "./styles/global.css";
 </script>
 
-<template>
-  <HelloWorld />
-</template>
+<template></template>
