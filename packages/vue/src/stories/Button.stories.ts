140851e0aaa6bb052b71d9ee743781c32f993f7f
Lukas <info@brainlesslukas.xyz>
2025-12-27 23:09:32 +0100

    feat: create Button fiale and render it in storybook

---
+++ packages/vue/src/stories/Button.stories.ts
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from "@storybook/vue3-vite";
+
+import Button from "../components/Button.vue";
+
+const meta = {
+  component: Button,
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  render: (args) => ({
+    components: { Button },
+    setup() {
+      return { args };
+    },
+    template: "<Button />",
+  }),
+};
