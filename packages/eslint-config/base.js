196a5b51350b5828bbe9a75340c6122aa7be38e9
Lukas <info@brainlesslukas.xyz>
2026-01-28 16:00:00 +0100

    fix: eslint types

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 
+/** @type {import('eslint').Linter.Config[]} */
 export const baseConfig = [
   js.configs.recommended,
   ...tseslint.configs.recommended,
