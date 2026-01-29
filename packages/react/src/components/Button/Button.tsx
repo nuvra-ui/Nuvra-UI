9aa60c7a86d6ec1feba794c6cbdafc648aa39cc7
Lukas <info@brainlesslukas.xyz>
2026-01-29 21:57:07 +0100

    impl: import utils package

--- packages/react/src/components/Button/Button.tsx
+++ packages/react/src/components/Button/Button.tsx
@@ -1,3 +1,5 @@
+import { cn } from "@nuvra-ui/utils";
+
 export function Button() {
-  return <div className="bg-green-600">Button</div>;
+  return <div className={cn("bg-green-400", "bg-amber-500")}>Button</div>;
 }
