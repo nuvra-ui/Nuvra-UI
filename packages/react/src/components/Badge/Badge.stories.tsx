0fecf17ffb2f037ac463f366e7442ac9e529d7aa
Lukas <info@brainlesslukas.xyz>
2026-01-31 14:20:39 +0100

    feat: create component and render in storybook

---
+++ packages/react/src/components/Badge/Badge.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Badge } from "./Badge";
+
+const meta = {
+  component: Badge,
+} satisfies Meta<typeof Badge>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Example: Story = {
+  args: {
+    children: "Badge",
+  },
+};
