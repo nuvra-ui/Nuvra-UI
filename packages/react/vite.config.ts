05018b23761dd88daa692231d0e44545ed05eb73
Lukas <info@brainlesslukas.xyz>
2026-02-05 22:16:12 +0100

    fix: export types

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -4,7 +4,7 @@ import dts from "vite-plugin-dts";
 import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [react(), dts({ insertTypesEntry: true, copyDtsFiles: false })],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
