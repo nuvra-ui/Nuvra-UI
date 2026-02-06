ad601652346579999e40abaadd57257f3300d6f7
Lukas <info@brainlesslukas.xyz>
2026-02-07 00:01:58 +0100

    fix: types

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,1 +1,3 @@
-export * from "./components/Button/Button";
+export { Badge } from "./components/Badge/Badge";
+export { Button } from "./components/Button/Button";
+export { Link } from "./components/Link/Link";
