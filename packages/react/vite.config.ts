e3836f91234e3c86af29d36e43616a37adf9e307
Lukas <info@brainlesslukas.xyz>
2026-02-12 21:26:36 +0100

    fix: bundler

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -4,12 +4,12 @@ import dts from "vite-plugin-dts";
 import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [react(), dts({ insertTypesEntry: true, rollupTypes: true })],
+  plugins: [react(), dts({ rollupTypes: true })],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
-      name: "Nuvra-UI",
-      fileName: (format) => `index.${format}.js`,
+      formats: ["es"],
+      fileName: "index",
     },
     rollupOptions: {
       external: ["react", "react-dom"],
