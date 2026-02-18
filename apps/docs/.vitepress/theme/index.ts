f155cbdaab9601e94d4c49788d1fa91ac1e92a6b
Lukas <info@brainlesslukas.xyz>
2026-02-18 14:38:59 +0100

    feat: register componnent & styles

--- apps/docs/.vitepress/theme/index.ts
+++ apps/docs/.vitepress/theme/index.ts
@@ -1,10 +1,12 @@
 // https://vitepress.dev/guide/custom-theme
 import { h } from "vue";
 import type { Theme } from "vitepress";
+import { Tab, Tabs } from "vue3-tabs-component";
 import DefaultTheme from "vitepress/theme";
 import "virtual:group-icons.css";
 import "./style.css";
 import "./custom.css";
+import "@red-asuka/vitepress-plugin-tabs/dist/style.css";
 
 export default {
   extends: DefaultTheme,
@@ -14,6 +16,14 @@ export default {
     });
   },
   enhanceApp({ app, router, siteData }) {
+    app.component("Tab", Tab);
+    app.component("Tabs", (props, ctx) =>
+      h(
+        Tabs,
+        { navClass: "tabs-component-tabs vp-raw", ...ctx.attrs, ...props },
+        ctx.slots
+      )
+    );
     // ...
   },
 } satisfies Theme;
