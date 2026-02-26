b831f9383183c970f42c7eead5f577bead091d4b
Lukas <info@brainlesslukas.xyz>
2026-02-26 22:07:26 +0100

    fix: export status component

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -3,3 +3,4 @@ export { Button } from "./components/Button/Button";
 export { Link } from "./components/Link/Link";
 export { Switch } from "./components/Switch/Switch";
 export { Textarea } from "./components/Textarea/Textarea";
+export { Status } from "./components/Status/Status"
