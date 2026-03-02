b8e4953b650b95cc65ff0ed234fc56e1007337d2
Lukas <info@brainlesslukas.xyz>
2026-03-02 21:29:18 +0100

    feat: favicon

--- apps/www/app/layout.tsx
+++ apps/www/app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo.svg" />
+      </head>
       <body className={rubik.className}>{children}</body>
     </html>
   );
