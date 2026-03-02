3ceeb97f41124e667c0a39b31b455d8b44a17ce6
Lukas <info@brainlesslukas.xyz>
2026-03-02 21:15:56 +0100

    feat: hero section text

---
+++ apps/www/components/sections/hero-section.tsx
@@ -0,0 +1,14 @@
+const HeroSection = () => {
+  return (
+    <div className="flex flex-col items-center justify-center w-full text-center">
+      <h1 className="text-7xl md:text-9xl font-bold bg-linear-to-r from-teal-400 to-yellow-200 bg-clip-text text-transparent">
+        Nuvra-UI
+      </h1>
+      <p className="-mt-3 text-center text-gray-700 text-base md:text-lg">
+        Build flexible, unstyled components - your design, your way.
+      </p>
+    </div>
+  );
+};
+
+export default HeroSection;
