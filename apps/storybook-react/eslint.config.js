5c82c3ee40ff116a87aafde89ddf5889a6f112c1
Lukas <info@brainlesslukas.xyz>
2026-03-02 12:53:25 +0100

    fix: lint on commit, small type fixes etc

--- apps/storybook-react/eslint.config.js
+++ apps/storybook-react/eslint.config.js
@@ -1,3 +1,14 @@
 import { baseConfig } from "@nuvra-ui/eslint-config";
 
-export default [...baseConfig];
+export default [
+  ...baseConfig,
+  {
+    ignores: [
+      "dist/**",
+      "build/**",
+      ".next/**",
+      "storybook-static/**",
+      "node_modules/**",
+    ],
+  },
+];
