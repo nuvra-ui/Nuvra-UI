25a0b2ac83179303df5ba95deb92d6eec5fedd07
Lukas <info@brainlesslukas.com>
2026-03-30 20:36:03 +0200

    fix: responsive footer

--- apps/www/components/layout/footer.tsx
+++ apps/www/components/layout/footer.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <div className="bg-gray-50 border border-gray-300 flex justify-center items-center">
-      <div className="p-4 pt-8 flex max-w-7xl w-full grid grid-cols-4 gap-4">
+      <div className="p-4 pt-8 flex max-w-7xl w-full flex-col sm:grid sm:grid-cols-4 gap-4">
         <div className="text-lg flex flex-row items-start gap-2">
           <span>
             <Image src="/logo.svg" alt="Logo" width={25} height={25} />
