09921d5ae58ab072b6e3ffae47c1cc5b3503c1f7
Lukas <info@brainlesslukas.com>
2026-03-30 20:57:52 +0200

    feat: add mit license and creator name

--- apps/www/components/layout/footer.tsx
+++ apps/www/components/layout/footer.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
+import { FaHeart } from "react-icons/fa";
+import { FaRegCopyright } from "react-icons/fa6";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <div className="bg-gray-50 border border-gray-300 flex justify-center items-center">
+    <div className="bg-gray-50 border border-gray-300 flex justify-center items-center flex-col">
       <div className="p-4 pt-8 flex max-w-7xl w-full flex-col sm:grid sm:grid-cols-4 gap-4">
         <div className="text-lg flex flex-row items-start gap-2">
           <span>
@@ -35,6 +39,20 @@ const Footer = () => {
             <a>Imprint</a>
           </div>
         </div>
+      </div>
+      <div className="mt-2 bg-gray-300 flex max-w-7xl w-full h-[1px]" />
+
+      <div className="max-w-7xl p-4 flex-col-reverse sm:flex-row-reverse flex w-full justify-between text-sm text-gray-400">
+        <p className="flex items-center gap-1">
+          Made with{" "}
+          <span className="text-red-600">
+            <FaHeart />
+          </span>{" "}
+          by brainlesslukas
+        </p>
+        <p className="flex items-center gap-1">
+          MIT {currentYear} <FaRegCopyright /> Nuvra-UI
+        </p>
       </div>
     </div>
   );
